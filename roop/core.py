e3c866fb994ba3907fe9857d535ed27213fe2a38
tommy7783 <154760933+tommy7783@users.noreply.github.com>
2023-12-24 12:18:06 -0800

    Update core.py

--- roop/core.py
+++ roop/core.py
@@ -149,8 +149,8 @@ def start() -> None:
             update_status('Processing to image failed!')
         return
     # process image to videos
-    if predict_video(roop.globals.target_path):
-        destroy()
+    # if predict_video(roop.globals.target_path):
+    #     destroy()
     update_status('Creating temporary resources...')
     create_temp(roop.globals.target_path)
     # extract frames
